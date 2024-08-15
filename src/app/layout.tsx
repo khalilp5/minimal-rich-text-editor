e88f6bfdfda7133b2f9119b2f79a887450d46919
Khalil <khalilp@geekyants.com>
2024-08-15 20:14:49 +0530

    Add UI component and theme toggle button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/theme-toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "dark:dark")}>
+        <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+          <ModeToggle className="fixed right-4 top-4" />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
